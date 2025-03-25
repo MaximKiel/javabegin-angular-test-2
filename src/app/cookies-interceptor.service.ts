2f6973dd1b971dd84d2adb7782ec58e7d70fedcf
MaximKiel <loup.du.nord@yandex.ru>
2025-03-25 17:25:07 +0300

    Ad commentaries into cookies-interceptor.service.ts

--- src/app/cookies-interceptor.service.ts
+++ src/app/cookies-interceptor.service.ts
@@ -7,14 +7,16 @@ import {
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+// для настройки всех исходящих запросов
 @Injectable()
 export class CookiesInterceptor implements HttpInterceptor {
 
   constructor() {}
 
+  // intercept - это аналог фильтров в веб приложении
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     request = request.clone({
-      withCredentials: true,
+      withCredentials: true, // браузер будет добавлять куки
     });
 
     return next.handle(request);
